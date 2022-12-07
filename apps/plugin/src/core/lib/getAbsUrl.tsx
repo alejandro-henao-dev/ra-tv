1ee43544526523079dd7fa30226f240ab8c13bd8
Alejandro Henao <ahenaoes@gmail.com>
2022-12-07 00:14:08 -0500

    plugin: dev
    fix image saving on entry gallery

--- apps/plugin/src/core/lib/getAbsUrl.tsx
+++ apps/plugin/src/core/lib/getAbsUrl.tsx
@@ -2,7 +2,7 @@ export const getAbsUrl = (url: string) => {
   const host = window.location.host
   const protocol = window.location.protocol
   
-  if (url.includes(host) && url.includes(protocol)) {
+  if ( url.includes(protocol)) {
     return url
   }
 
